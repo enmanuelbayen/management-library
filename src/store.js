172d5f8a7765fea3e061a9e76a1d7632d684e6d7
enmanuelbayen <enmanuel0495@gmail.com>
2023-06-12 17:24:00 -0400

    finish exercise

--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './redux/cart/cartSlice';
-
+import modalReducer from './redux/modal/modalSlice';
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    modal: modalReducer,
   },
 });
